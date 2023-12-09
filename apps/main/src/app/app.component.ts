50760ce9f8b8498f0beaad55a4837382923b607e
fleish80 <Fleish.D@gmail.com>
2023-12-09 22:15:38 +0200

    new 17

--- apps/main/src/app/app.component.ts
+++ apps/main/src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'angular-new-seventeen-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, MatToolbarModule, RouterLink, RouterLinkActive, MatButtonModule],
+  selector: 'df-root',
+  template: `
+
+      <mat-toolbar color="primary">
+          <a routerLink="defer-loading" routerLinkActive="active" mat-flat-button color="primary">Defer Loading</a>
+      </mat-toolbar>
+      <router-outlet/>
+
+
+    <router-outlet/>
+  `,
 })
 export class AppComponent {
-  title = 'main';
 }
--- apps/main/src/app/app.routes.ts
+++ apps/main/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', redirectTo: 'defer-loading', pathMatch: 'full' },
+  { path: 'defer-loading', loadComponent: () => import('./defer-loading/defer-loading.component') }
+];
